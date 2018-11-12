c033b81cf9434020348b2395146c9c5974a2260b
PiotrKulpa <p.kulpa@onet.eu>
2018-11-12 10:02:30 +0100

    feat: add sorting method

--- src/App.js
+++ src/App.js
@@ -7,6 +7,38 @@ class App extends Component {
     posts: []
   }
 
+  
+
+  sortPosts(e) {
+    switch(e.target.value) {
+      case 'newest':
+        console.log('przelaczylem na najnowsze');
+        this.setState({
+          posts: this.state.posts.sort((a,b) => (a.date < b.date) ? 1 : ((b.date < a.date) ? -1 : 0))
+        })
+        break;
+      case 'oldest':
+        console.log('przelaczylem na najstarsze');
+        this.setState({
+          posts: this.state.posts.sort((a,b) => (a.date > b.date) ? 1 : ((b.date > a.date) ? -1 : 0))
+        })
+        break;
+      case 'asc':
+        console.log('przelaczylem na alfabetycznie ');
+        this.setState({
+          posts: this.state.posts.sort((a,b) => (a.title.rendered > b.title.rendered) ? 1 : ((b.title.rendered > a.title.rendered) ? -1 : 0))
+        })
+        break;
+      case 'desc':
+        console.log('przelaczylem na alfabetycznie od tyłu');
+        this.setState({
+          posts: this.state.posts.sort((a,b) => (a.title.rendered < b.title.rendered) ? 1 : ((b.title.rendered < a.title.rendered) ? -1 : 0))
+        })
+        break;
+
+    }
+  }
+
   componentDidMount() {
     fetch('http://localhost/wprest/wp/wp-json/wp/v2/posts?per_page=100')
     .then((response) => {
@@ -28,11 +60,22 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        Szukaj: <input />
+        Sortuj:
+        <select onChange={(e) => {this.sortPosts(e)}}>
+          <option value="newest">Najnowsze</option>
+          <option value="oldest">Najstarsze</option>
+          <option value="asc">Alfabetycznie rosnąco</option>
+          <option value="desc">Alfabetycznie malejąco</option>
+        </select>
+
         {!this.state.posts ? <p>Nie ma wpisów</p> : this.state.posts.map((el) =>
 
           <div key={el.id}>
             <h1>{el.title.rendered}</h1>
+            <p>{el.date}</p>
             <div dangerouslySetInnerHTML={{__html: el.excerpt.rendered}} />
+            <a href="#">More</a>
             <div style={{ display: "none"}} dangerouslySetInnerHTML={{__html: el.content.rendered}} />
 
           </div>
